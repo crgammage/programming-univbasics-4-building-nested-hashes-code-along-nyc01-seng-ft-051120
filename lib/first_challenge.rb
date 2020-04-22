95308aad8793de7ce1151db42f7ce28b675d9acb
Christa Gammage <crgammage@gmail.com>
2020-04-22 22:25:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,10 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {}
+       :patriarch => {
+         :name => "Lord Montague"
+         :age =>
+       }
        :matriarch => {}
        :hero => {}
        :hero_friends => []
