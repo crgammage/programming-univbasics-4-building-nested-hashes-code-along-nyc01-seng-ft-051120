8e9562993d68a8823a60d6c04ffea670269021e5
Christa Gammage <crgammage@gmail.com>
2020-04-22 22:31:00 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,8 @@ def first_challenge
          :age => "15",
          :status => "alive"
        }
-       :hero_friends => [name: "Benvolio", age: "17", attitude: "worried"]
+       :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}
+       {name: "Mercutio", age: "18", attitude: "hot-headed"}]
      }
      :capulet => {
        :patriarch => {
@@ -30,7 +31,8 @@ def first_challenge
          :age => "15",
          :status => "alive"
        }
-       :heroine_friends => []
+       :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}
+       {name: "Nurse", age: "44", attitude: "worried"}]
      }
    }
 end
